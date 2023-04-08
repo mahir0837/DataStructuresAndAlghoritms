dba3baa7477a841beff97184ec45f2e43e3af678
owner <mahirsarac89@gmail.com>
2023-04-08 17:54:05 -0400

    recursive fibbonacci

---
+++ dsa-Stacks/src/main/java/com/sarac/RecursiveFibonacci.java
@@ -0,0 +1,16 @@
+package com.sarac;
+
+public class RecursiveFibonacci {
+
+    public static void main(String[] args) {
+
+        System.out.println(fib(6));
+
+    }
+
+    public static int fib(int n){
+        if (n==0)return 0;
+        if (n==1)return 1;
+        return fib(n-1)+fib(n-2);
+    }
+}
